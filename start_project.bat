6ff4bab197550a50ac11c7cc56bb124e4c1e877f
lpmuebleria <lpmuebleria2026@gmail.com>
2026-03-08 11:35:51 -0600

    fix: update local startup script to use virtual environment

--- start_project.bat
+++ start_project.bat
@@ -6,7 +6,7 @@ echo.
 
 :: Abrir el Backend en una nueva ventana
 echo [1/2] Iniciando Servidor Backend (Python/FastAPI)...
-start cmd /k "uvicorn main:app --host 127.0.0.1 --port 8000 --reload"
+start cmd /k "call venv\Scripts\activate && uvicorn main:app --host 127.0.0.1 --port 8000 --reload"
 
 :: Esperar un momento para asegurar que el server inicie
 timeout /t 5 /nobreak >nul
